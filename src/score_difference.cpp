a8b87107aa3c1b2c8197d061846f5dd73aad0865
German Telmo Eizaguirre <gteizaguirre@gmail.com>
2021-06-02 16:33:36 +0200

    Result analysis functions + corrected score difference error

--- src/score_difference.cpp
+++ src/score_difference.cpp
@@ -42,29 +42,31 @@ NumericVector mean_diff_cpp(NumericVector bins1, NumericVector counts1, NumericV
 /*** R
 N = 1000000
 
-bins1 <- runif(N)
-counts1 <- sample.int(20, N, replace = TRUE)
-bins2 <- runif(N)
-counts2 <- sample.int(20, N, replace = TRUE)
+# bins1 <- runif(N)
+# counts1 <- sample.int(20, N, replace = TRUE)
+# bins2 <- runif(N)
+# counts2 <- sample.int(20, N, replace = TRUE)
+# 
+# res_cpp <- mean_diff_cpp(bins1, counts1, bins2, counts2)
+# cdat_cpp <- data.frame(Set = c("Prone", "Resistant"), Score = c(res_cpp[0], res_cpp[1]))
+# 
+# mean_diff_R <- function(){
+# 
+#   p=unlist(sapply(1:length(bins1), function(i) rep(bins1[i],counts1[i])))
+#   r=unlist(sapply(1:length(bins2), function(i) rep(bins2[i],counts2[i])))
+# 
+#   dat1 <- data.frame(Set = factor(rep(c("Prone"), each=length(p))), Score = p)
+# 
+#   dat2 <- data.frame(Set = factor(rep(c("Resistant"), each=length(r))), Score = r)
+# 
+#   dat <- rbind(dat1,dat2)
+#   cdat <- ddply(dat, "Set", summarise, rating.mean=mean(Score))
+# 
+#   return(cdat)
+# }
+# 
+# cdat_R <- mean_diff_R()
 
-res_cpp <- mean_diff_cpp(bins1, counts1, bins2, counts2)
-cdat_cpp <- data.frame(Set = c("Prone", "Resistant"), Score = c(res_cpp[0], res_cpp[1]))
-
-mean_diff_R <- function(){
-
-  p=unlist(sapply(1:length(bins1), function(i) rep(bins1[i],counts1[i])))
-  r=unlist(sapply(1:length(bins2), function(i) rep(bins2[i],counts2[i])))
-
-  dat1 <- data.frame(Set = factor(rep(c("Prone"), each=length(p))), Score = p)
-
-  dat2 <- data.frame(Set = factor(rep(c("Resistant"), each=length(r))), Score = r)
-
-  dat <- rbind(dat1,dat2)
-  cdat <- ddply(dat, "Set", summarise, rating.mean=mean(Score))
-
-  return(cdat)
-}
-
-cdat_R <- mean_diff_R()
+res <-mean_diff_cpp(pro_bin, pro_count, res_bin, res_count)
 
 */
